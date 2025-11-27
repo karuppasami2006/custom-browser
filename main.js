568859ae98b79097f974a31c946c644aee3edadc
Im-arun11 <arun2006ui@gmail.com>
2025-11-27 21:48:20 +0530

    browser update

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    backgroundColor: "#1b1b1d",
+    titleBarStyle: "hiddenInset",
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      webviewTag: true,         // <-- IMPORTANT: allow <webview>
-      contextIsolation: true    }
+      webviewTag: true,
+      contextIsolation: true
+    }
   });
 
   win.loadFile('index.html');
